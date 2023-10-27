553b55bbacbd1d662622df82502427965e43c45a
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-10-27 14:15:46 -0400

    updates save message functions

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-//import { suggestions } from "./suggestions.js";
+
+import { suggestions } from "./suggestions.js"
+
 const nameInput = document.getElementById('name')
 const emailInput = document.getElementById('email')
 const subjectInput = document.getElementById('subject')
@@ -9,9 +11,11 @@ let recieptientName;
 let recieptientEmail;
 let subject;
 let gratitudeMessage;
+let localStorageArray = [];
 
 
-nameInput.addEventListener('change', (e)=>{
+
+nameInput.addEventListener('change', (e) => {
     recieptientName = e.target.value;
 
 })
@@ -20,7 +24,7 @@ emailInput.addEventListener('change', (e) => {
     recieptientEmail = e.target.value;
 })
 
-subjectInput.addEventListener('change', (e)=>{
+subjectInput.addEventListener('change', (e) => {
     subject = e.target.value
 })
 
@@ -29,16 +33,93 @@ messageInput.addEventListener('change', (e) => {
 })
 
 
-submitBtn.addEventListener('click', (e)=>{
+submitBtn.addEventListener('click', (e) => {
     e.preventDefault()
+    // mailTo()
+    saveEmail()
+    addToSaved()
 
-    console.log(recieptientName)
-    console.log(recieptientEmail)
-    console.log(subject)
-    console.log(gratitudeMessage)
-    mailTo()
 })
 
 function mailTo() {
     window.location.href = `mailto:${recieptientEmail}?subject=${subject}&body=${gratitudeMessage}`;
+}
+
+function saveEmail() {
+
+    const obj = {
+        name: recieptientName,
+        subject: subject,
+        email: recieptientEmail,
+        message: gratitudeMessage
+    }
+
+    const newEntry = JSON.stringify(obj)
+    localStorage.setItem(
+        recieptientName,
+        newEntry
+    )
+    return
+}
+
+
+
+
+// function allStorage() {
+//     for (let i = 0; i < localStorage.length; i++) {
+//         let entry = localStorage.getItem(localStorage.key(i));
+//         localStorageArray.push(JSON.parse(entry))
+//     }
+// }
+
+
+
+// window.addEventListener('load', () => {
+//     allStorage()
+// })
+
+
+
+
+const suggestionsSection = document.querySelector('#suggestions')
+
+suggestions.forEach((suggestion) => {
+
+    const cardContainer = document.createElement('div')
+    cardContainer.classList.add('card')
+    const cardBody = document.createElement('div')
+    cardBody.classList.add('card-body')
+
+    const subjectLine = document.createElement('h6')
+    subjectLine.classList.add("card-subtitle")
+    subjectLine.innerText = suggestion.subject;
+    const message = document.createElement('p')
+    message.classList.add('card-text')
+    message.innerText = suggestion.message
+    cardBody.append( subjectLine, message)
+    cardContainer.append(cardBody)
+    suggestionsSection.append(cardContainer)
+
+})
+
+
+
+function addToSaved(){
+
+    const savedSection = document.querySelector('#saved')
+    const cardContainer = document.createElement('div')
+    cardContainer.classList.add('card')
+    const cardBody = document.createElement('div')
+    cardBody.classList.add('card-body')
+    const nameField = document.createElement('h5')
+    nameField.innerText = recieptientName;
+    const subjectLine = document.createElement('h6')
+    subjectLine.classList.add("card-subtitle")
+    subjectLine.innerText = subject;
+    const message = document.createElement('p')
+    message.classList.add('card-text')
+    message.innerText = gratitudeMessage;
+    cardBody.append(nameField, subjectLine, message)
+    cardContainer.append(cardBody)
+    savedSection.append(cardContainer)
 }
--- suggestions.js
+++ suggestions.js
@@ -1,23 +1,31 @@
 const suggestions = [
     {
         subject: "Thanks for Your Support",
-        message: "Hi [Friend/Family Member's Name], I just wanted to take a moment to express my heartfelt gratitude for your unwavering support.Your kindness means the world to me. Thank you! Warm regards, [Your Name]"
+        message: "Hi [Friend/Family Member's Name], \n I just wanted to take a moment to express my heartfelt gratitude for your unwavering support.Your kindness means the world to me. Thank you! Warm regards,\n [Your Name]"
     },
     {
         subject: "Grateful for You",
-        message: "Dear [Friend/Family Member's Name], I wanted to let you know how grateful I am to have you in my life. Your presence and positivity always brighten my days. With appreciation, [Your Name]"
+        message: "Dear [Friend/Family Member's Name], \n I wanted to let you know how grateful I am to have you in my life. Your presence and positivity always brighten my days. With appreciation,\n [Your Name]"
     },
     {
         subject: "You're Amazing!",
-        message: "Hey [Friend/Family Member's Name], I just wanted to remind you that you are absolutely amazing.Your constant support and love never go unnoticed. Thanks a million, [Your Name]"
+        message: "Hey [Friend/Family Member's Name], \n I just wanted to remind you that you are absolutely amazing.Your constant support and love never go unnoticed. Thanks a million, \n [Your Name]"
     },
     {
         subject: "Thank You for Being You",
-        message: "Hi [Friend/Family Member's Name], Your authenticity and kindness make you one of a kind.I'm so thankful for you and the wonderful person you are. With love, "
+        message: "Hi [Friend/Family Member's Name], \n Your authenticity and kindness make you one of a kind.I'm so thankful for you and the wonderful person you are. With love, \n [Your Name] "
     },
     {
         subject: "Your Help Made My Day",
-        message: "Dear [Friend/Family Member's Name], Your help today made a world of difference.I appreciate your willingness to lend a hand when I needed it most.Thank you! All the best, [Your Name]"
+        message: "Dear [Friend/Family Member's Name], \n Your help today made a world of difference.I appreciate your willingness to lend a hand when I needed it most.Thank you! All the best, \n [Your Name]"
+    },
+    {
+        subject: "Appreciating Your Friendship",
+        message: "Dear [Friend/Family Member's Name], \n I just wanted to take a moment to express how much I appreciate our friendship. Your friendship enriches my life in so many ways. Gratefully,\n [Your Name]"
+    },
+    {
+        subject: "Thanks for the Memories",
+        message: "Dear [Friend/Family Member's Name], \n I cherish all the wonderful memories we've created together. Thank you for the great times and for being a part of my life. Sincerely, \n [Your Name]"
     },
 ]
 
