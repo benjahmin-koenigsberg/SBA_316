05f3d9f6a56b61cfab52a44fc647a79ddb4b23d7
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-10-27 12:06:07 -0400

    updates emailto funcitonality

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-import { suggestions } from "./suggestions.js";
+//import { suggestions } from "./suggestions.js";
 const nameInput = document.getElementById('name')
-const emailInput = document.getElementById('name')
-const messageInput = document.getElementById('name')
+const emailInput = document.getElementById('email')
+const subjectInput = document.getElementById('subject')
+const messageInput = document.getElementById('message')
 const submitBtn = document.getElementById('submitBtn')
 
 let recieptientName;
 let recieptientEmail;
+let subject;
 let gratitudeMessage;
+
 
 nameInput.addEventListener('change', (e)=>{
     recieptientName = e.target.value;
@@ -17,17 +20,25 @@ emailInput.addEventListener('change', (e) => {
     recieptientEmail = e.target.value;
 })
 
+subjectInput.addEventListener('change', (e)=>{
+    subject = e.target.value
+})
+
 messageInput.addEventListener('change', (e) => {
     gratitudeMessage = e.target.value;
 })
 
 
-submitBtn.addEventListener('click', mailTo)
+submitBtn.addEventListener('click', (e)=>{
+    e.preventDefault()
 
-function mailTo() {
-    window.location.href = `mailto:${recieptientEmail}&body=${gratitudeMessage}`;
     console.log(recieptientName)
     console.log(recieptientEmail)
+    console.log(subject)
     console.log(gratitudeMessage)
+    mailTo()
+})
 
+function mailTo() {
+    window.location.href = `mailto:${recieptientEmail}?subject=${subject}&body=${gratitudeMessage}`;
 }
