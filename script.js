67e0c417acd64aea3b8e1d8015e522a5d7bdcff4
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-10-27 20:44:08 -0400

    adds read and write to storage function

--- script.js
+++ script.js
@@ -5,107 +5,15 @@ const nameInput = document.getElementById('name')
 const emailInput = document.getElementById('email')
 const subjectInput = document.getElementById('subject')
 const messageInput = document.getElementById('message')
+const sender = document.getElementById('sendersName')
 const submitBtn = document.getElementById('submitBtn')
 const saveBtn = document.querySelector('#saveBtn')
 const suggestionsSection = document.querySelector('#suggestions')
 const savedSection = document.querySelector('#saved')
 
+const savedArray = [];
 
-
-let recieptientName;
-let recieptientEmail;
-let subject;
-let gratitudeMessage;
-let savedArray = [];
-
-
-nameInput.addEventListener('blur', (e) => {
-    recieptientName = e.target.value;
-
-})
-
-emailInput.addEventListener('blur', (e) => {
-    recieptientEmail = e.target.value;
-})
-
-subjectInput.addEventListener('blur', (e) => {
-    subject = e.target.value
-})
-
-messageInput.addEventListener('blur', (e) => {
-    gratitudeMessage = e.target.value;
-})
-
-
-submitBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-
-    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-    if (!recieptientEmail.match(emailRegex)) {
-        window.alert('Please enter a valid email address')
-        return
-    }
-    if ( (!recieptientName) || (!subject) || (!gratitudeMessage) ) {
-        window.alert('Please fill in all fields')
-        return
-        } else {
-        mailTo()
-        nameInput.value = '';
-        emailInput.value = '';
-        subjectInput.value = '';
-        messageInput.value = '';
-        }
-})
-
-
-
-
-
-saveBtn.addEventListener('click', (e) => {
-
-    e.preventDefault()
-
-    if ((!recieptientName) || (!recieptientEmail) || (!subject) || (!gratitudeMessage)) {
-        window.alert('Please fill in all fields')
-        return
-    }
-
-            const obj = {
-                name: recieptientName,
-                subject: subject,
-                email: recieptientEmail,
-                message: gratitudeMessage
-            }
-            savedArray.push(obj)
-
-            savedArray.forEach((entry) => {
-
-                const cardContainer = document.createElement('div')
-                cardContainer.classList.add('card')
-                const cardBody = document.createElement('div')
-                cardBody.classList.add('card-body')
-                const nameField = document.createElement('h5')
-                nameField.innerText = entry.name
-                nameField.classList.add('card-title')
-                const subjectLine = document.createElement('h6')
-                subjectLine.classList.add("card-subtitle")
-                subjectLine.innerText = entry.subject
-                const message = document.createElement('p')
-                message.classList.add('card-text')
-                message.innerText = entry.message
-                cardBody.append(nameField, subjectLine, message)
-                cardContainer.append(cardBody)
-                savedSection.append(cardContainer)
-            })
-            nameInput.value = '';
-            emailInput.value = '';
-            subjectInput.value = '';
-            messageInput.value = '';
-
-
-})
-
+const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 
 suggestions.forEach((suggestion) => {
@@ -116,28 +24,176 @@ suggestions.forEach((suggestion) => {
     cardContainer.append(cardBody)
     //use first child method
     cardContainer.firstChild.classList.add('card-body')
+    const subjectDiv = document.createElement('div')
+    subjectDiv.classList.add('subjectDiv')
+    const copyIcon = document.createElement('i')
+    copyIcon.classList.add("fa-clipboard", "fa")
     const subjectLine = document.createElement('h6')
+
+    subjectDiv.append(subjectLine, copyIcon)
+
     subjectLine.classList.add("card-subtitle", 'subject-text', 'text-center')
     subjectLine.innerText = suggestion.subject;
 
-    subjectLine.addEventListener('click', () => {
-        navigator.clipboard.writeText(subjectLine.innerText);
-    })
 
+    const messageDiv = document.createElement('div')
+    const copyIcon2 = document.createElement('i')
+    copyIcon2.classList.add("fa-clipboard", "fa")
+    messageDiv.classList.add('messageDiv')
     const message = document.createElement('p')
+
+    messageDiv.append(message, copyIcon2)
+
     message.classList.add('card-text', 'message-text')
     message.innerText = suggestion.message
 
-    message.addEventListener('click', () => {
-    navigator.clipboard.writeText(message.innerText);
+    copyIcon.addEventListener('click', () => {
+        navigator.clipboard.writeText(subjectLine.innerText);
+        subjectInput.value = suggestion.subject
+
     })
 
-    cardBody.append(subjectLine, message)
+    copyIcon2.addEventListener('click', () => {
+        navigator.clipboard.writeText(message.innerText);
+        messageInput.value = `Dear ${nameInput.value} \n ${suggestion.message} \n Sincerly ${sender.value}`
+    })
+
+
+    cardBody.append(subjectDiv, messageDiv)
     suggestionsSection.append(cardContainer)
 
 })
 
 
 function mailTo() {
-    window.location.href = `mailto:${recieptientEmail}?subject=${subject}&body=${gratitudeMessage}`;
+    window.location.href = `mailto:${emailInput.value}?subject=${subjectInput.value}&body=${messageInput.value}`;
 }
+
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+
+
+    if (!emailInput.value.match(emailRegex)) {
+        window.alert('Please enter a valid email address')
+        return
+    }
+
+    if ((!nameInput.value) || (!emailInput.value) || (!subjectInput.value) || (!messageInput.value)) {
+        window.alert('Please fill in all fields')
+        return
+    }
+     else {
+        mailTo()
+        nameInput.value = '';
+        emailInput.value = '';
+        subjectInput.value = '';
+        messageInput.value = '';
+    }
+})
+
+
+
+
+
+saveBtn.addEventListener('click', (e) => {
+
+    e.preventDefault()
+
+    saveItem()
+
+    if (!emailInput.value.match(emailRegex)) {
+        window.alert('Please enter a valid email address')
+        return
+    }
+
+    if ( (!nameInput.value) || (!emailInput.value) || (!subjectInput.value) || (!messageInput.value) ) {
+        window.alert('Please fill in all fields')
+        return
+    }
+
+    const cardContainer = document.createElement('div')
+    cardContainer.classList.add('card')
+    const cardBody = document.createElement('div')
+    cardBody.classList.add('card-body')
+    const nameField = document.createElement('h5')
+    nameField.innerText = nameInput.value
+    nameField.classList.add('card-title')
+    const subjectLine = document.createElement('h6')
+    subjectLine.innerText = subjectInput.value
+    subjectLine.classList.add("card-subtitle")
+    const message = document.createElement('p')
+    message.classList.add('card-text')
+    message.innerText = messageInput.value
+    cardBody.append(nameField, subjectLine, message)
+    cardContainer.append(cardBody)
+    savedSection.append(cardContainer)
+
+})
+    // const obj = {
+    //     name: nameInput.value,
+    //     subject: subjectInput.value,
+    //     email: emailInput.value,
+    //     message: messageInput.value
+    // }
+    // savedArray.push(obj)
+
+
+
+
+function saveItem(){
+
+    const item = {
+        name: nameInput.value,
+        subject: subjectInput.value,
+        email: emailInput.value,
+        message: messageInput.value
+    }
+
+    localStorage.setItem(nameInput.value, JSON.stringify(item))
+
+}
+
+
+
+function getStorage(){
+    //let storageArray = [];
+
+    for (let i = 0; i <= localStorage.length; i++) {
+        let item = localStorage.getItem(localStorage.key(i));
+        if ( item !== "INFO" && item !== null && item !== undefined)
+            savedArray.push(JSON.parse(item));
+    }
+
+
+}
+
+
+window.addEventListener('load', ()=>{
+    getStorage()
+
+
+    savedArray.forEach((entry) => {
+
+        const cardContainer = document.createElement('div')
+        cardContainer.classList.add('card')
+        const cardBody = document.createElement('div')
+        cardBody.classList.add('card-body')
+        const nameField = document.createElement('h5')
+        nameField.innerText = entry.name
+        nameField.classList.add('card-title')
+        const subjectLine = document.createElement('h6')
+        subjectLine.classList.add("card-subtitle")
+        subjectLine.innerText = entry.subject
+        const message = document.createElement('p')
+        message.classList.add('card-text')
+        message.innerText = entry.message
+        cardBody.append(nameField, subjectLine, message)
+        cardContainer.append(cardBody)
+        savedSection.append(cardContainer)
+    })
+
+    console.log(savedArray)
+
+
+})
--- suggestions.js
+++ suggestions.js
@@ -1,31 +1,31 @@
 const suggestions = [
     {
         subject: "Thanks for Your Support",
-        message: "Hi       , \n I just wanted to take a moment to express my heartfelt gratitude for your unwavering support.Your kindness means the world to me. Thank you! Warm regards,\n        "
+        message: "       \nI just wanted to take a moment to express my heartfelt gratitude for your unwavering support.Your kindness means the world to me. Thank you! Warm regards,\n        "
     },
     {
         subject: "Grateful for You",
-        message: "Dear       , \n I wanted to let you know how grateful I am to have you in my life. Your presence and positivity always brighten my days. With appreciation,\n        "
+        message: "      \nI wanted to let you know how grateful I am to have you in my life. Your presence and positivity always brighten my days. With appreciation,\n        "
     },
     {
         subject: "You're Amazing!",
-        message: "Hey       , \n I just wanted to remind you that you are absolutely amazing.Your constant support and love never go unnoticed. Thanks a million, \n        "
+        message: "       \nI just wanted to remind you that you are absolutely amazing.Your constant support and love never go unnoticed. Thanks a million, \n        "
     },
     {
         subject: "Thank You for Being You",
-        message: "Hi       , \n Your authenticity and kindness make you one of a kind.I'm so thankful for you and the wonderful person you are. With love, \n         "
+        message: "       \nYour authenticity and kindness make you one of a kind.I'm so thankful for you and the wonderful person you are. With love, \n         "
     },
     {
         subject: "Your Help Made My Day",
-        message: "Dear       , \n Your help today made a world of difference.I appreciate your willingness to lend a hand when I needed it most.Thank you! All the best, \n        "
+        message: "      \nYour help today made a world of difference.I appreciate your willingness to lend a hand when I needed it most.Thank you! All the best, \n        "
     },
     {
         subject: "Appreciating Your Friendship",
-        message: "Dear       , \n I just wanted to take a moment to express how much I appreciate our friendship. Your friendship enriches my life in so many ways. Gratefully,\n        "
+        message: "       \nI just wanted to take a moment to express how much I appreciate our friendship. Your friendship enriches my life in so many ways. Gratefully,\n        "
     },
     {
         subject: "Thanks for the Memories",
-        message: "Dear       , \n I cherish all the wonderful memories we've created together. Thank you for the great times and for being a part of my life. Sincerely, \n        "
+        message: "       \nI cherish all the wonderful memories we've created together. Thank you for the great times and for being a part of my life. Sincerely, \n        "
     },
 ]
 
